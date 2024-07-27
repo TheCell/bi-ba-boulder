d50df379c9cab7ce2d2cfb66a5ae09ad0b16b69c
Simon Hischier <simon_hischier@hotmail.com>
2024-07-27 23:41:13 +0200

    first rendering working

---
+++ src/app/boulder-render/boulder-render.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boulder-render',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './boulder-render.component.html',
+  styleUrl: './boulder-render.component.scss',
+})
+export class BoulderRenderComponent { }
