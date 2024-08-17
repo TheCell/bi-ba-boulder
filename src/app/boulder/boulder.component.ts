19d659eeb693860a06e493567d9e9e8e944ec040
TheCell <simon.hischier@gmail.com>
2024-08-17 23:22:02 +0200

    3d model and image

--- src/app/boulder/boulder.component.ts
+++ src/app/boulder/boulder.component.ts
@@ -1,25 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { BoulderRenderComponent } from "../boulder-render/boulder-render.component";
 
 @Component({
   selector: 'app-boulder',
   standalone: true,
   imports: [
-    CommonModule 
-  ],
+    CommonModule,
+    BoulderRenderComponent,
+    BoulderRenderComponent
+],
   templateUrl: './boulder.component.html',
-  styleUrl: './boulder.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './boulder.component.scss'
 })
 export class BoulderComponent {
   public title: string;
   public boulderImageUrl: SafeStyle;
   public image: string;
+  public modelLoaded = false;
 
   public constructor(private domSanitizer: DomSanitizer) {
     this.title = 'test';
     this.image = '/test-images/bimano-image.webp';
     this.boulderImageUrl = this.domSanitizer.bypassSecurityTrustStyle('url(/test-images/bimano-image.webp)');
+
+    setTimeout(() => {
+      // faking for now
+      this.modelLoaded = true;
+    }, 2000);
   }
 }
