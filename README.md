216cec90e01a7a2d520638ff5fac2c8868b058d1
Simon Hischier <simon_hischier@hotmail.com>
2024-10-13 22:01:04 +0200

    generic loading working

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 The next todos:
 
-[] Figure a route dto out
-[] login
-[] 3D scan 2 object with LOD process
-[] Load LOD levels
-[] Offline compatible
-[] setup cdn
+[x] Figure a route dto out  
+[] login  
+[] 3D scan 2 object with LOD process  
+[x] Load LOD levels  
+[] Offline compatible  
+[] setup cdn  
 
 # helpful resources
 Generate interface etc. from json: https://app.quicktype.io/
@@ -15,5 +15,5 @@ Generate interface etc. from json: https://app.quicktype.io/
 to simulate the database locally I use XAMPP
 
 # The Backend
-The Backend is build with PHP, MySQL
+The Backend is build with PHP, MySQL   
 I'm using Symfony for the Backend and Composer to create the Files etc.
--- src/app/boulder/boulder.component.ts
+++ src/app/boulder/boulder.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnDestroy } from '@angular/core';
 import { BoulderLegendComponent } from '../components/boulder-legend/boulder-legend.component';
 import { BoulderRenderComponent } from '../boulder-render/boulder-render.component';
-import { SafeStyle, DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { Subject, Subscription } from 'rxjs';
-import { BlocDto, DefaultService } from '../api';
+import { Subject, Subscription, switchMap } from 'rxjs';
+import { BlocDto } from '../api';
 import { BoulderLine } from '../interfaces/boulder-line';
 import { ResolutionLevel } from '../interfaces/resolution-level';
 import { BoulderLoaderService } from '../background-loading/boulder-loader.service';
@@ -23,65 +22,55 @@ import { BoulderLoaderService } from '../background-loading/boulder-loader.servi
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BoulderComponent implements OnDestroy {
-  public boulderImageUrl: SafeStyle;
-  public image: string;
   public bloc: BlocDto;
 
   public currentRawModel?: ArrayBuffer = undefined;
   public currentLines: BoulderLine[] = [];
 
-  private finishedLoading = new Subject<void>();
+  private loadNextResolution = new Subject<void>();
+  private startLoadingBoulder = new Subject<void>();
   private subscription = new Subscription();
   private blocId: string | null;
-  private boulderUrl: string;
-  private nextResolutionToLoad?: ResolutionLevel;
+  private boulderUrl = '';
+  private resolutionToLoad?: ResolutionLevel;
 
-  private defaultService = inject(DefaultService);
   private boulderLoaderService = inject(BoulderLoaderService);
 
   public constructor(
-    private domSanitizer: DomSanitizer,
-    private activatedRoute: ActivatedRoute) {
+    private activatedRoute: ActivatedRoute,
+    private changeDetectorRef: ChangeDetectorRef) {
       this.bloc = this.activatedRoute.snapshot.data['bloc'];
       this.blocId = this.activatedRoute.snapshot.paramMap.get('id');
-      this.image = './test-images/Bloc_5.jpg';
-      this.boulderImageUrl = this.domSanitizer.bypassSecurityTrustStyle('url(./test-images/Bloc_5.jpg)');
 
-      this.subscription.add(this.finishedLoading.subscribe({
+      this.subscription.add(this.loadNextResolution.subscribe({
         next: () => {
-          if (this.nextResolutionToLoad !== undefined) {
-            const urlAndInfo = this.boulderLoaderService.getUrl(this.bloc, this.nextResolutionToLoad);
-            this.nextResolutionToLoad = urlAndInfo.higherResolution;
+          if (this.resolutionToLoad !== undefined) {
+            const urlAndInfo = this.boulderLoaderService.getUrl(this.bloc, this.resolutionToLoad);
+            this.resolutionToLoad = urlAndInfo.higherResolution;
             this.boulderUrl = urlAndInfo.url;
 
             if (this.boulderUrl.length > 0) {
-              this.boulderLoaderService.loadBoulder(this.boulderUrl).subscribe({
-                next: (data: ArrayBuffer) => {
-                  this.currentRawModel = data;
-                  this.finishedLoading.next();
-                },
-              });
+              this.startLoadingBoulder.next();
             }
           }
         }
       }));
 
+      this.subscription.add(this.startLoadingBoulder.pipe(switchMap(() => this.boulderLoaderService.loadBoulder(this.boulderUrl))).subscribe({
+        next: (data: ArrayBuffer) => {
+          this.currentRawModel = data;
+          this.loadNextResolution.next();
+          this.changeDetectorRef.markForCheck();
+        },
+      }));
+
       const urlAndInfo = this.boulderLoaderService.getUrl(this.bloc);
-      this.nextResolutionToLoad = urlAndInfo.higherResolution;
+      this.resolutionToLoad = urlAndInfo.higherResolution;
       this.boulderUrl = urlAndInfo.url;
-      this.finishedLoading.next();
+      this.startLoadingBoulder.next();
   }
 
   public ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
-
-  // private loadBoulder(): void {
-  //   this.defaultService.loadTestDaoneBoulder2('medium').subscribe({
-  //   next: (data) => {
-  //   this.currentRawModel = data;
-  //   this.finishedLoading.next('medium');
-  //   }
-  //   });
-  // }
 }
