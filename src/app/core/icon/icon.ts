60a01aa5567b9ace6fecfaa3960505d37ce36617
Simon Hischier <simon_hischier@hotmail.com>
2025-11-30 00:28:47 +0100

    added loading icon, fixed style

---
+++ src/app/core/icon/icon.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  imports: [],
+  templateUrl: './icon.html',
+  styleUrl: './icon.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Icon {
+  
+}
