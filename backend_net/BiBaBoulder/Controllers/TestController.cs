d8c547d741ab7cc8e1e31f6af4e98419e920b6ce
Simon Hischier <simon_hischier@hotmail.com>
2026-01-30 12:16:55 +0100

    database working

---
+++ backend_net/BiBaBoulder/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Thecell.Bibaboulder.Model;
+
+namespace Thecell.Bibaboulder.BiBaBoulder.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly BiBaBoulderDbContext _dbContext;
+
+    public TestController(BiBaBoulderDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet("test")]
+    public IActionResult GetTest()
+    {
+        var count = _dbContext.Spraywalls.Count();
+        return Ok(new { SpraywallCount = count });
+    }
+}
