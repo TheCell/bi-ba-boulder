989ff048a28780930529b9ef365d07fed77d2a28
Simon Hischier <simon_hischier@hotmail.com>
2026-01-30 15:22:06 +0100

    moved config and fixed stuff

--- backend_net/Model/BiBaBoulderDbContext.cs
+++ backend_net/Model/BiBaBoulderDbContext.cs
@@ -1,19 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Thecell.Bibaboulder.Model.Model;
 
-namespace Thecell.Bibaboulder.Model
+namespace Thecell.Bibaboulder.Model;
+
+public class BiBaBoulderDbContext : DbContext
 {
-    public class BiBaBoulderDbContext : DbContext
-      {
-        public BiBaBoulderDbContext(DbContextOptions<BiBaBoulderDbContext> options): base(options)
-        { }
+    public BiBaBoulderDbContext(DbContextOptions<BiBaBoulderDbContext> options): base(options)
+    { }
 
-        public DbSet<Spraywall> Spraywalls { get; set; }
+    public DbSet<Spraywall> Spraywalls { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //  modelBuilder.Entity<Spraywall>()
-        //    .HasKey(s => s.Id);
-        //}
-      }
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //  modelBuilder.Entity<Spraywall>()
+    //    .HasKey(s => s.Id);
+    //}
 }
