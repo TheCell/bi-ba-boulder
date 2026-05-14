6021b923506da9c6820ab86997695ec2589d4c90
Simon Hischier <simon_hischier@hotmail.com>
2026-05-14 19:30:45 +0200

    updated dependencies and fixed editing (#69)
    * WIP setup dotnet backend
    
    * updated progress
    
    * Starting over
    
    * Setup API without authentication for now
    
    * WIP .net 10 buildup
    
    * renaming, fixed migration
    
    * database working
    
    * updated copliot settings
    
    * testing the porting
    
    * moved config and fixed stuff
    
    * WIP setting up copilot instructions. Fixed CS project
    
    * added existing entities
    
    * added CQRS examples and handling
    
    * WIP test setup
    
    * WIP Setup Testing
    
    * refactoring
    
    * minor fixes
    
    * more small fixes
    
    * THIS SHIT FIXED IT
    
    * added more tests
    
    * Fixed routes not found
    
    * Fixed all Tests
    
    * Testing V7
    
    * basic authorization setup
    
    * WIP bff oidc
    
    * rework login with bff
    
    * fixed redirect
    
    * fixed errors
    
    * finished user authorization
    
    * WIP OpenApi dotnet
    
    * added mcp and updated readme
    
    * generating sources working
    
    * Changed typescript generation
    
    * added assertions and builders
    
    * added dto and mappings
    
    * WIP porting
    
    * fixing
    
    * added and testing bloc controller
    
    * spraywall and problem search
    
    * testing spraywallsController
    
    * tested UsersController
    
    * spraywalls controller test
    
    * SpraywallProblem Controller Test
    
    * updated structure
    
    * forgot to add the integration test
    
    * get sector test
    
    * removed unused
    
    * added feedback endpoint
    
    * added lines controller
    
    * added administration controller
    
    * Finished first draft
    
    * generate frontend services
    
    * endpoints json
    
    * switch to api-net
    
    * more switching
    
    * fixing generating
    
    * backend & frontend basically working
    
    * fixed startup error
    
    * local auth mock
    
    * switching to https and extra security
    
    * referenced roles
    
    * fixed login and problem displaying
    
    * implemented fileStorageClient abstraction
    
    * fixed tests not running
    
    * added mailservice
    
    * added image compression
    
    * added migration
    
    * renamed methods
    
    * renamed email table
    
    * fixed missing rename
    
    * Porting to new Website
    
    * removed php
    
    * removed php references
    
    * added skills
    
    * removed more references
    
    * minor fixes
    
    * fixing backend test?
    
    * fixing backend
    
    * fixing linting
    
    * package error
    
    * more fixin
    
    * updated frontend
    
    * fixed stuff
    
    * update FE
    
    * updated things
    
    * update packages
    
    * more fixing
    
    * fixed test
    
    * fixed db date
    
    * fixed dependency
    
    * fixed ef core step
    
    * more fixing
    
    * this might fix it
    
    * fixed the ef core update too
    
    * added quality
    
    * temp disable migration application
    
    * new file-storage path
    
    * filestorage update
    
    * changed folders around
    
    * more config mumbo jumbo
    
    * some fixing
    
    * fixed all errors
    
    * dependencie updates
    
    * fixed editing
    
    * updated errorhandling
    
    * fixed local mail test
    
    * updated package-lock

--- src/app/core/interceptors/error-handler-interceptor.ts
+++ src/app/core/interceptors/error-handler-interceptor.ts
@@ -2,6 +2,7 @@ import { HttpContextToken, HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequ
 import { Observable } from 'rxjs';
 import { ToastService } from '../toast-container/toast.service';
 import { inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 export const SKIP_ERROR_HANDLER = new HttpContextToken<boolean>(() => false);
 
@@ -19,25 +20,30 @@ export const errorHandlerInterceptor: HttpInterceptorFn = (req: HttpRequest<unkn
           observer.next(event);
         },
         error: (err: HttpErrorResponse) => {
-          console.error('HTTP Error occurred:', err);
           const title = `Error: ${err.status} (${err.statusText})`;
           let message = '';
-
-          if (err.status === 401) {
-            message = 'You are not logged in or your session has expired.';
-          } else if (err.status === 403) {
-            message = 'You do not have permission to perform this action.';
-          } else if (err.error) {
-            const error = err.error;
-            if (typeof error.error === 'string') {
-              message = message.concat(`${error.error}`);
-            } else if (error && typeof error.message === 'string') {
-              message = message.concat(`${error.message}`);
+          
+          if (environment.production) {
+            if (err.status === 401) {
+              message = 'You are not logged in or your session has expired.';
+            } else if (err.status === 403) {
+              message = 'You do not have permission to perform this action.';
+            } else if (err.status === 500) {
+              message = 'A server error occurred. Please try again later.';
+            } else if (err.error) {
+              const error = err.error;
+              if (typeof error.error === 'string') {
+                message = message.concat(`${error.error}`);
+              } else if (error && typeof error.message === 'string') {
+                message = message.concat(`${error.message}`);
+              } else {
+                message = 'An error occurred while processing your request.';
+              }
             } else {
               message = 'An error occurred while processing your request.';
             }
           } else {
-            message = 'An error occurred while processing your request.';
+            message = err.error.message;
           }
 
           toastService.showDanger(title, message);
