ebae2a1a83ef32cff99b728f05fa344ed7f52e85
Simon Hischier <simon_hischier@hotmail.com>
2026-07-10 14:42:02 +0200

    added migration

---
+++ backend_net/Migrations/Migrations/20260710124036_UpdatedLines.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiBaBoulder.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedLines : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Points");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Lines");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Data",
+                table: "Lines",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FontGrade",
+                table: "Lines",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "Lines");
+
+            migrationBuilder.DropColumn(
+                name: "FontGrade",
+                table: "Lines");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Lines",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Points",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LineId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Version = table.Column<long>(type: "bigint", nullable: false),
+                    X = table.Column<double>(type: "float", nullable: false),
+                    Y = table.Column<double>(type: "float", nullable: false),
+                    Z = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Points", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Points_Lines_LineId",
+                        column: x => x.LineId,
+                        principalTable: "Lines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Points_LineId",
+                table: "Points",
+                column: "LineId");
+        }
+    }
+}
