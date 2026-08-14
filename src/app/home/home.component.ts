bd88c30ccfcbb8c05e9df56677116f37af3a9b17
Simon Hischier <simon_hischier@hotmail.com>
2026-08-14 09:23:36 +0200

    fixed home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,10 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FeedbackOverlay } from '../core/feedback-overlay/feedback-overlay';
 import { OutdoorAreasService, SpraywallsService, OutdoorAreaDto, SpraywallDto } from '@api-net/index';
-import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, FeedbackOverlay, NgStyle],
+  imports: [RouterLink, FeedbackOverlay],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -17,8 +16,6 @@ export class HomeComponent {
   public readonly patternTiles: readonly number[] = Array.from({ length: 12 }, (_, index: number) => index);
   public spraywalls = signal<SpraywallDto[]>([]);
   public outdoorAreas = signal<OutdoorAreaDto[]>([]);
-  public spraywallTileNumber = `${Math.floor(Math.random() * 83)}`.padStart(3, '0');
-  public outdoorAreaTileNumber = `${Math.floor(Math.random() * 83)}`.padStart(3, '0');
 
   constructor() {
     this.spraywallsService.getSpraywalls().subscribe({
